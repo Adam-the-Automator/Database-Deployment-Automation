6f1e922fef0f79725569a1b3ec294ab0ac65e9b8
Adam Bertram <adbertram@gmail.com>
2021-06-06 10:19:36 -0500

    Update and rename CheckInventoryTable.ps1 to CheckInventoryTable.Tests.ps1

--- tests/CheckInventoryTable.Tests.ps1
+++ tests/CheckInventoryTable.Tests.ps1
@@ -4,30 +4,25 @@ param (
                 HelpMessage="SQL Server name in Azure.")]
     [string]
     $sqlServerName,
-
     [Parameter(Mandatory=$true,
             HelpMessage="SQL Database name in Azure.")]
     [string]
     $sqlDatabaseName,
-
     [Parameter(Mandatory=$true,
             HelpMessage="SQL administrator for connection to SQL server in Azure.")]
     [string]
     $sqlAdministrator,
-
     [Parameter(Mandatory=$true,
             HelpMessage="SQL password for connection to SQL server in Azure.")]
     [string]
     $sqlPassword
 )
-
-
 Describe "Check inventory table" {
     It "Gets inventory table records" {
         $credPwd = ConvertTo-SecureString $sqlPassword -AsPlainText -Force
         $credential = New-Object System.Management.Automation.PSCredential ($sqlAdministrator, $credPwd)
         $query = "SELECT [ServerName], [Description] FROM dbo.DatabaseInventory"
-        $result = Invoke-DbaQuery -SqlInstance $sqlServerName -Database $sqlDatabaseName -SqlCredential $credential -Query $query
+        $result = (Invoke-DbaQuery -SqlInstance $sqlServerName -Database $sqlDatabaseName -SqlCredential $credential -Query $query)[0]
         $result.Count | Should -BeGreaterOrEqual 1
     }
 }
